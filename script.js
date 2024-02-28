775e4aed9340959313c8214f034a32b5ba860aa2
Shreya Jain <shreya11072003@gmail.com>
2024-02-28 22:07:20 +0530

    commit

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const svgPlay=`<svg class="masterPlay_1" fill="white" xmlns="http://www.w3.org/2
 const svgPlay1=`<svg id="play" width="15px" class="masterPlay_2" width="20px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9V168c0-8.7 4.7-16.7 12.3-20.9z"/></svg>`
 let i=0;
 
-let audioElement=new Audio('/song/song2.mp3');
+let audioElement=new Audio('./song/song2.mp3');
 let songIndex=0;
 let curr=0;
-let next= songIndex+1;;
+let next= songIndex+1;
 let prev;
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
